c274583cc7aca203533981990f3bc3f46dce0dca
Eric Liu <51763643+EricL521@users.noreply.github.com>
2024-09-15 23:42:32 -0400

    Created basic home page

--- src/routes/home/home-page.tsx
+++ src/routes/home/home-page.tsx
@@ -3,7 +3,13 @@ import styles from './home-page.module.css';
 function HomePage() {
 	return (
 		<div id={styles["home-page"]}>
-			<h1>Home</h1>
+			<div className={styles["paragraph"]}>
+				<h1>Hello! You've made it to my website!</h1>
+				<p>
+					&#9;This website is all about me (Eric)! This page is temperary, but maybe take a look at the projects page until this one's done. 
+					That's why I'm making this website ; ).
+				</p>
+			</div>
 		</div>
 	);
 }
