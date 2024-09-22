3d848ed4dffac6ad50a06c765502d0827932e588
Eric Liu <51763643+EricL521@users.noreply.github.com>
2024-09-22 15:42:12 -0400

    Reworded some projects

--- src/page/projects/projects-page.tsx
+++ src/page/projects/projects-page.tsx
@@ -11,7 +11,7 @@ function ProjectsPage() {
 				<h1 id={styles["projects-label"]} className={transitionStyles["interactive"]}>Featured Projects</h1>
 				<p id={styles["projects-description"]} className={transitionStyles["interactive"]}>Some coding projects that I think are pretty cool</p>
 				<div id={styles["projects-div"]}>
-					<Project name="Schmek" description="A multiplayer snake game with abilities" 
+					<Project name="Schmek" description="A twist on the classic snake game, where players can upgrade abilities and create simple combos" 
 						demoLink="https://schmek.dkg.zone" codeLink="https://github.com/EricL521/Schmek"/>
 					<Project name="RingVC" description="A discord bot that allows you to easily ping people to join you in a Discord server voice chat" 
 						codeLink="https://github.com/EricL521/RingVC"/>
@@ -19,7 +19,7 @@ function ProjectsPage() {
 						demoLink="https://ericl521.github.io/Rotating-Arrow-Game/" codeLink="https://github.com/EricL521/Rotating-Arrow-Game"/>
 					<Project name="ParticleJS" description="In my opinion, satisfying, but very ineffecient particle simulation. Warning: flash bang for dark mode users" 
 						demoLink="https://ericl521.github.io/particlejs/test/test.html" codeLink="https://github.com/EricL521/particlejs"/>
-					<Project name="Soldier Tycoon" description="One of my first games! A one file monstrocity coupled with a lot of alerts at the beginning" 
+					<Project name="Soldier Tycoon" description="One of my first games! A one file monstrocity where you defend a castle from raiders" 
 						demoLink="https://ericl521.github.io/Soldier-Tycoon/" codeLink="https://github.com/EricL521/Soldier-Tycoon"/>
 				</div>
 			</div>
