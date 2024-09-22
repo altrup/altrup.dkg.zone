ca582fc27810a245707cd5452a3de695d5df9baf
Eric Liu <51763643+EricL521@users.noreply.github.com>
2024-09-21 23:07:17 -0400

    Updated project text

--- src/page/projects/projects-page.tsx
+++ src/page/projects/projects-page.tsx
@@ -9,7 +9,7 @@ function ProjectsPage() {
 		<Element id={styles["projects-page"]} name="projects">
 			<div className={transitionStyles["interactive"]}>
 				<h1 id={styles["projects-label"]} className={transitionStyles["interactive"]}>Featured Projects</h1>
-				<p id={styles["projects-description"]} className={transitionStyles["interactive"]}>Well documented projects that I think are pretty cool</p>
+				<p id={styles["projects-description"]} className={transitionStyles["interactive"]}>Some coding projects that I think are pretty cool</p>
 				<div id={styles["projects-div"]}>
 					<Project name="Schmek" description="A multiplayer snake game with abilities" 
 						demoLink="https://schmek.dkg.zone" codeLink="https://github.com/EricL521/Schmek"/>
@@ -19,7 +19,7 @@ function ProjectsPage() {
 						demoLink="https://ericl521.github.io/Rotating-Arrow-Game/" codeLink="https://github.com/EricL521/Rotating-Arrow-Game"/>
 					<Project name="ParticleJS" description="In my opinion, satisfying, but very ineffecient particle simulation. Warning: flash bang for dark mode users" 
 						demoLink="https://ericl521.github.io/particlejs/test/test.html" codeLink="https://github.com/EricL521/particlejs"/>
-					<Project name="Soldier Tycoon" description="One of my first games! A one file monstrocity filled with a cringe 8th grade good student agreement" 
+					<Project name="Soldier Tycoon" description="One of my first games! A one file monstrocity coupled with a lot of alerts at the beginning" 
 						demoLink="https://ericl521.github.io/Soldier-Tycoon/" codeLink="https://github.com/EricL521/Soldier-Tycoon"/>
 				</div>
 			</div>
