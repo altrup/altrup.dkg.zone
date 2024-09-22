8f87cd13574384ae8ad2429efae68f8c9882d96b
Eric Liu <51763643+EricL521@users.noreply.github.com>
2024-09-22 15:26:12 -0400

    Created simple contacts page

--- src/page/contacts/contacts-page.tsx
+++ src/page/contacts/contacts-page.tsx
@@ -1,12 +1,25 @@
+import { useMemo } from "react";
 import { Element } from "react-scroll";
 
 import styles from "./contacts-page.module.css";
+import transitionStyles from '../transitions.module.css';
 
 function ContactsPage() {
+	const interactiveClass = useMemo(() => [styles["interactive"], transitionStyles["interactive"]].join(' '), [transitionStyles]);
+	const clickableInteractiveClass = useMemo(() => [interactiveClass, transitionStyles["clickable"]].join(' '), [interactiveClass, transitionStyles]);
 	return (
 		<Element id={styles["contacts-page"]} name='contacts'>
-			<h1>Contacts</h1>
-			
+			<div className={interactiveClass}>
+				<div id={styles["contacts-page-title"]}>
+					<h1 className={interactiveClass}>Contacts</h1>
+					<p className={interactiveClass}>and Links</p>
+				</div>
+				<div id={styles["contacts"]} className={interactiveClass}>
+					<a href="mailto:altrup.dkg.zone" className={clickableInteractiveClass}>altrup@dkg.zone</a>
+					<a href="mailto:e255liu@uwaterloo.ca" className={clickableInteractiveClass}>e255liu@uwaterloo.ca</a>
+					<a href="https://github.com/EricL521" target="_blank" className={clickableInteractiveClass}>GitHub</a>
+				</div>
+			</div>
 		</Element>
 	);
 }
