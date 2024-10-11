14f740bf12f6dd8d83e9b4b8bf28b1fb0db3f6ea
Eric Liu <51763643+EricL521@users.noreply.github.com>
2024-10-11 01:48:14 -0400

    Added some more projects

--- src/page/projects/components/project.tsx
+++ src/page/projects/components/project.tsx
@@ -4,24 +4,33 @@ import { useMemo } from "react";
 import styles from "./project.module.css";
 import transitionStyles from '../../transitions.module.css';
 
-function Project({name, description, demoLink, codeLink, image}: {name: string, description: string, demoLink?: string, codeLink: string, image?: string}) {
+function Project({name, description, demoLink, codeLink, image}: {
+	name: string, description: string, demoLink?: string, codeLink?: string, 
+	image?: {
+		src: string, height: number, alt?: string
+	}
+}) {
 	const clickableInteractiveClass = useMemo(() => [transitionStyles["interactive"], transitionStyles["clickable"]].join(' '), [transitionStyles]);
 	return (
 		<div className={styles["project"]}>
-			<div className={transitionStyles["interactive"]}>
+			<div className={[styles["project-child"], transitionStyles["interactive"]].join(' ')}>
 				{image?
-					<img src={image} />
+					<img className={styles["image"]} alt={image.alt} src={image.src} height={image.height} />
 				: undefined}
-				<div className={styles["label"]}>
-					<h2 className={[transitionStyles["interactive"], styles["title"]].join(' ')}>{name}</h2>
-					<div className={styles["links"]}>
-						{demoLink?
-							<a className={clickableInteractiveClass} href={demoLink} target="_blank">Demo</a>
-						: undefined}
-						<a className={clickableInteractiveClass} href={codeLink} target="_blank">Code</a>
+				<div className={styles["text"]}>
+					<div className={styles["label"]}>
+						<h3 className={[transitionStyles["interactive"], styles["title"]].join(' ')}>{name}</h3>
+						<div className={styles["links"]}>
+							{demoLink?
+								<a className={clickableInteractiveClass} href={demoLink} target="_blank">Demo</a>
+							: undefined}
+							{codeLink?
+								<a className={clickableInteractiveClass} href={codeLink} target="_blank">Code</a>
+							: undefined}
+						</div>
 					</div>
+					<p className={[styles["description"], transitionStyles["interactive"]].join(' ')}>{description}</p>
 				</div>
-				<p className={[styles["description"], transitionStyles["interactive"]].join(' ')}>{description}</p>
 			</div>
 		</div>
 	)
--- src/page/projects/projects-page.tsx
+++ src/page/projects/projects-page.tsx
@@ -8,19 +8,31 @@ function ProjectsPage() {
 	return (
 		<Element id={styles["projects-page"]} name="projects">
 			<div className={transitionStyles["interactive"]}>
-				<h1 id={styles["projects-label"]} className={transitionStyles["interactive"]}>Featured Projects</h1>
-				<p id={styles["projects-description"]} className={transitionStyles["interactive"]}>Some coding projects that I think are pretty cool</p>
-				<div id={styles["projects-div"]}>
-					<Project name="Schmek" description="A twist on the classic snake game, where players can upgrade abilities and create simple combos" 
-						demoLink="https://schmek.dkg.zone" codeLink="https://github.com/EricL521/Schmek"/>
-					<Project name="RingVC" description="A discord bot that allows you to easily ping people to join you in a Discord server voice chat" 
-						codeLink="https://github.com/EricL521/RingVC"/>
-					<Project name="Rotating Arrow Game" description="An online implementation of a really cool puzzle game I found in a mobile app" 
-						demoLink="https://ericl521.github.io/Rotating-Arrow-Game/" codeLink="https://github.com/EricL521/Rotating-Arrow-Game"/>
-					<Project name="ParticleJS" description="In my opinion, satisfying, but very ineffecient particle simulation. Warning: flash bang for dark mode users" 
-						demoLink="https://ericl521.github.io/particlejs/test/test.html" codeLink="https://github.com/EricL521/particlejs"/>
-					<Project name="Soldier Tycoon" description="One of my first games! A one file monstrocity where you defend a castle from raiders" 
-						demoLink="https://ericl521.github.io/Soldier-Tycoon/" codeLink="https://github.com/EricL521/Soldier-Tycoon"/>
+				<h1 id={styles["projects-label"]} className={transitionStyles["interactive"]}>Projects</h1>
+				<p id={styles["projects-description"]} className={transitionStyles["interactive"]}>Some projects that I think are pretty cool</p>
+
+				<div className={[styles["project-type-div"], transitionStyles["interactive"]].join(' ')}>
+					<h2 className={[styles["project-type-label"], transitionStyles["interactive"]].join(' ')}>Coding</h2>
+					<div className={styles["projects-div"]}>
+						<Project name="Schmek" description="A twist on the classic snake game, where players can upgrade abilities and create simple combos" 
+							demoLink="https://schmek.dkg.zone" codeLink="https://github.com/EricL521/Schmek"/>
+						<Project name="RingVC" description="A discord bot that allows you to easily ping people to join you in a Discord server voice chat" 
+							codeLink="https://github.com/EricL521/RingVC"/>
+						<Project name="Rotating Arrow Game" description="An online implementation of a really cool puzzle game I found in a mobile app" 
+							demoLink="https://ericl521.github.io/Rotating-Arrow-Game/" codeLink="https://github.com/EricL521/Rotating-Arrow-Game"/>
+						<Project name="ParticleJS" description="In my opinion, satisfying, but very ineffecient particle simulation. Warning: flash bang for dark mode users" 
+							demoLink="https://ericl521.github.io/particlejs/test/test.html" codeLink="https://github.com/EricL521/particlejs"/>
+						<Project name="Soldier Tycoon" description="One of my first games! A one file monstrocity where you defend a castle from raiders" 
+							demoLink="https://ericl521.github.io/Soldier-Tycoon/" codeLink="https://github.com/EricL521/Soldier-Tycoon"/>
+					</div>
+				</div>
+
+				<div className={[styles["project-type-div"], transitionStyles["interactive"]].join(' ')}>
+					<h2 className={[styles["project-type-label"], transitionStyles["interactive"]].join(' ')}>Other Stuff</h2>
+					<div className={styles["projects-div"]}>
+						<Project name="Custom Dice" description="Custom wood dice made with a CNC and a custom jig. Designed using Inkscape because I like open source stuff." 
+							image={{src: "/project-images/custom-dice.jpg", alt: "Picture of a die", height: 200}}/>
+					</div>
 				</div>
 			</div>
 		</Element>
