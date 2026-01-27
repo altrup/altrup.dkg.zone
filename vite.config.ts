ab8b3f3d7710f78b1b17a374f5d040bb699119b3
altrup <51763643+altrup@users.noreply.github.com>
2026-01-27 02:14:40 -0500

    Add ability to use local sections.json file

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,68 @@
-import { defineConfig, loadEnv } from "vite";
+import fs from "fs/promises";
+import path from "path";
+import { defineConfig, loadEnv, ViteDevServer } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 import { getSections } from "./src/helper-functions/getSections";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
+  let sections;
+
+  const sectionsFilePath = path.normalize(
+    path.join(__dirname, env.SECTIONS_JSON_FILE),
+  );
+  if (!isProduction) {
+    console.log(`Loading sections from ${sectionsFilePath}`);
+    try {
+      sections = JSON.parse(
+        await fs.readFile(sectionsFilePath, "utf8"),
+      ) as Section[];
+    } catch (e) {
+      console.log(`Failed to load sections from ${sectionsFilePath}`, e);
+    }
+  }
+
+  if (!sections) {
+    console.log("Loading sections from supabase");
+
+    sections = await getSections({
+      supabaseAnonKey: env.SUPABASE_ANON_KEY,
+      supabaseURL: env.SUPABASE_URL,
+      supabaseTableName: env.SUPABASE_TABLE_NAME,
+    });
+  }
+
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      !isProduction
+        ? {
+            name: "restart-on-json-change",
+            configureServer(server: ViteDevServer) {
+              // Manually add the file to the watcher
+              server.watcher.add(sectionsFilePath);
+
+              server.watcher.on("change", (file: string) => {
+                if (path.normalize(file) === sectionsFilePath) {
+                  console.log(
+                    "🔄 Sections JSON changed. Restarting Vite server...",
+                  );
+                  server.restart().catch((e: unknown) => {
+                    console.error(e);
+                  });
+                }
+              });
+            },
+          }
+        : undefined,
+    ],
     define: {
-      __SECTIONS__: await getSections({
-        supabaseAnonKey: env.SUPABASE_ANON_KEY,
-        supabaseURL: env.SUPABASE_URL,
-        supabaseTableName: env.SUPABASE_TABLE_NAME,
-      }),
+      __SECTIONS__: sections,
     },
   };
 });
