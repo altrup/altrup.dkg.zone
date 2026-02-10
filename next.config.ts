2bf070330841dac6b09ddda7fe07853cce452497
altrup <51763643+altrup@users.noreply.github.com>
2026-02-10 15:47:57 -0500

    Fix build issues

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  outputFileTracingIncludes: {
+    '/': ['./src/inline.css', './src/theme-manager.js'],
+  }
 };
 
 export default nextConfig;
