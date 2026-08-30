a277c9875321f988f66f57fc3d0ec10be6d9c0da
altrup <51763643+altrup@users.noreply.github.com>
2026-08-30 19:33:17 -0400

    always log supabase realtime events

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,9 +36,7 @@ export default async function RootLayout({
 				<meta name="color-scheme" content="dark light" />
 			</head>
 			<body className={styles["transition"]}>
-				<div id="mount">
-					{children}
-				</div>
+				<div id="mount">{children}</div>
 			</body>
 		</html>
 	);
